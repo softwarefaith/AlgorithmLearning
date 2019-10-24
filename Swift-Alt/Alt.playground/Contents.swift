579a66393c7963e2690fafd6d42d78dc78520cff
蔡杰 <jie.cai@mljr.com>
2019-10-24 16:31:50 +0800

    队列协议

--- Swift-Alt/Alt.playground/Contents.swift
+++ Swift-Alt/Alt.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+//https://github.com/raywenderlich/swift-algorithm-club
 var str:String = "Hello, playground"
 
 
---
+++ Swift-Alt/Swift-Alt/Source/Queue.swift
@@ -0,0 +1,28 @@
+//
+//  Queue.swift
+//  Swift-Alt
+//
+//  Created by 蔡杰 on 2019/10/24.
+//  Copyright © 2019 蔡杰. All rights reserved.
+//
+
+import Foundation
+
+/// 队列协议
+public protocol IQueue {
+    
+    associatedtype E
+    
+    var size:Int {get}
+    
+    func isEmpty() -> Bool;
+    
+    func clear();
+    
+    //入队
+    func enQueue(_ element:E)
+    //出队
+    func deQueue()->E?
+    //获取队列首元素
+    func front()->E?
+}
